b9f8fa337c26239eb775f2bf61a3d062a682c1ec
MeriemCh <chkirmeriem619@gmail.com>
2022-08-07 18:49:51 +0100

    first commit 7-08-2022

--- JS/index.js
+++ JS/index.js
@@ -107,3 +107,19 @@ window.addEventListener('load', () => {
   }
   
   window.onload = fadeOut();
+
+  // savoir plus 
+
+   
+let menu = document.querySelector('#menu-icon');
+ 
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    menu.classList.remove('bx-x');
+    
+ 
+}
